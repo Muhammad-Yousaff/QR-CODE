a02e92a417f3942ad9e2db7456cd02b63bde309d
Muhammad yousaf <muhammad.yousaf6239@gmail.com>
2025-12-07 10:02:46 -0800

    QR Code Generator

--- QR/views.py
+++ QR/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from . froms import QRCodeForm
-
+import qrcode
 def generate_qr_code(request):
-    form = QRCodeForm()
+    if request.method == 'POST':
+        form = QRCodeForm(request.POST)
+        if form.is_valid():
+            restaurant_name = form.cleaned_data['restaurant_name']
+            url = form.cleaned_data['url']
+
+           
+            qr=qrcode.make(url)
+            file.name= res_name.replace(" ",'_') + '_qr.png'
+            qr.save(file.name)
+
+
+    else:
+        form = QRCodeForm()
     context = {
         'form': form,
     }
